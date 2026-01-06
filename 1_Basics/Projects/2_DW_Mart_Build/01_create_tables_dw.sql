28cfe9bd5714b4ec3a306b5ee53a683dc992c552
rikkisingh01 <rikkisingh@gmail.com>
2026-01-05 20:54:18 -0800

    Add ETL queries for data warehouse

---
+++ 1_Basics/Projects/2_DW_Mart_Build/01_create_tables_dw.sql
@@ -0,0 +1,5 @@
+-- Drop existing tables if they exist (for item dependency)
+DROP TABLE IF EXISTS skills_job_dim;
+DROP TABLE IF EXISTS job_postings_fact;
+DROP TABLE IF EXISTS skills_dim;
+DROP TABLE IF EXISTS company_dim;
